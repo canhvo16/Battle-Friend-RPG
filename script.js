6da6e736f8f08dc6290c98fca00083de76ba768b
canhvo16 <canhvo16@gmail.com>
2022-03-08 14:15:40 -0600

    limited the amount of times a move can be made in a match

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ let hp1 = document.querySelector('.hp1')
 let attack1 = document.querySelector('.attack1')
 let defense1 = document.querySelector('.defense1')
 let player1Buttons = document.querySelectorAll('.player1')
+let player1Tag = document.querySelector('.player1Name')
+let hitButton1 = document.querySelector('.hit1')
+let smashButton1 = document.querySelector('.smash1')
+let powerButton1 = document.querySelector('.power1')
+let scareButton1 = document.querySelector('.scare1')
+let hit1 = 10
+let smash1 = 3
+let power1 = 5
+let scare1 = 5
 
 let player2Hit = document.querySelector('#move1b')
 let player2Smash = document.querySelector('#move2b')
@@ -15,6 +24,15 @@ let hp2 = document.querySelector('.hp2')
 let attack2 = document.querySelector('.attack2')
 let defense2 = document.querySelector('.defense2')
 let player2Buttons = document.querySelectorAll('.player2')
+let player2Tag = document.querySelector('.player2Name')
+let hitButton2 = document.querySelector('.hit2')
+let smashButton2 = document.querySelector('.smash2')
+let powerButton2 = document.querySelector('.power2')
+let scareButton2 = document.querySelector('.scare2')
+let hit2 = 10
+let smash2 = 3
+let power2 = 5
+let scare2 = 5
 
 let player1Turn = true
 let player2Turn = false
@@ -26,7 +44,7 @@ let player1 = {
   attack: 10,
   defense: 2,
   hit: function () {
-    if (player1Turn === true) {
+    if (player1Turn === true && hit1 > 0) {
       let damage = player1.attack - player2.defense
       player2.hp -= damage
       narration.innerHTML = `${player1.name} has hit ${player2.name}! ${player2.name}'s HP has gone down by ${damage} life points! Current HP is ${player2.hp}!`
@@ -34,11 +52,13 @@ let player1 = {
       setTimeout(player2Move, 3500)
       player1Turn = false
       player2Turn = true
+      hit1 -= 1
+      hitButton1.innerHTML = `${hit1}`
       checkWin()
     }
   },
   smash: function () {
-    if (player1Turn === true) {
+    if (player1Turn === true && smash1 > 0) {
       let damage = player1.attack * 1.5 - player2.defense
       player2.hp -= damage
       narration.innerHTML = `${player1.name} smashed ${player2.name}! ${player2.name}'s HP has gone down by ${damage} life points! Current HP is ${player2.hp}!`
@@ -46,27 +66,33 @@ let player1 = {
       setTimeout(player2Move, 3500)
       player1Turn = false
       player2Turn = true
+      smash1 -= 1
+      smashButton1.innerHTML = `${smash1}`
       checkWin()
     }
   },
   powerUp: function () {
-    if (player1Turn === true) {
+    if (player1Turn === true && power1 > 0) {
       player1.attack += 2
       narration.innerHTML = `${player1.name} has increased his/her/their strength! Power now at ${player1.attack}`
       attack1.innerHTML = `Attack Power: ${player1.attack}`
       setTimeout(player2Move, 3500)
       player1Turn = false
       player2Turn = true
+      power1 -= 1
+      powerButton1.innerHTML = `${power1}`
     }
   },
   breakDefense: function () {
-    if (player1Turn === true) {
+    if (player1Turn === true && scare1 > 0) {
       player2.defense -= 1
       narration.innerHTML = `${player1.name} has scared ${player2.name}! ${player2.name}'s defense has fallen to ${player2.defense}!`
       defense2.innerHTML = `Defense Power: ${player2.defense}`
       setTimeout(player2Move, 3500)
       player1Turn = false
       player2Turn = true
+      scare1 -= 1
+      scareButton1.innerHTML = `${scare1}`
     }
   }
 }
@@ -77,7 +103,7 @@ let player2 = {
   attack: 10,
   defense: 2,
   hit: function () {
-    if (player2Turn === true) {
+    if (player2Turn === true && hit2 > 0) {
       let damage = player2.attack - player1.defense
       player1.hp -= damage
       narration.innerHTML = `${player2.name} has hit ${player1.name}! ${player1.name}'s HP has gone down by ${damage} life points! Current HP is ${player1.hp}!`
@@ -85,11 +111,13 @@ let player2 = {
       setTimeout(player1Move, 3500)
       player1Turn = true
       player2Turn = false
+      hit2 -= 1
+      hitButton2.innerHTML = `${hit2}`
       checkWin()
     }
   },
   smash: function () {
-    if (player2Turn === true) {
+    if (player2Turn === true && smash2 > 0) {
       let damage = player2.attack * 1.5 - player1.defense
       player1.hp -= damage
       narration.innerHTML = `${player2.name} smashed ${player1.name}! ${player1.name}'s HP has gone down by ${damage} life points! Current HP is ${player1.hp}!`
@@ -97,35 +125,43 @@ let player2 = {
       setTimeout(player1Move, 3500)
       player1Turn = true
       player2Turn = false
+      smash2 -= 1
+      smashButton2.innerHTML = `${smash2}`
       checkWin()
     }
   },
   powerUp: function () {
-    if (player2Turn === true) {
+    if (player2Turn === true && power2 > 0) {
       player2.attack += 2
       narration.innerHTML = `${player2.name} has increased his/her/their strength! Power now at ${player2.attack}`
       attack2.innerHTML = `Attack Power: ${player2.attack}`
       setTimeout(player1Move, 3500)
       player1Turn = true
       player2Turn = false
+      power2 -= 1
+      powerButton2.innerHTML = `${power2}`
     }
   },
   breakDefense: function () {
-    if (player2Turn === true) {
+    if (player2Turn === true && scare2 > 0) {
       player1.defense -= 1
       narration.innerHTML = `${player2.name} has scared ${player1.name}! ${player1.name}'s defense has fallen to ${player1.defense}!`
       defense1.innerHTML = `Defense Power: ${player1.defense}`
       setTimeout(player1Move, 3500)
       player1Turn = true
       player2Turn = false
+      scare2 -= 1
+      scareButton2.innerHTML = `${scare2}`
     }
   }
 }
 
+player1Tag.innerHTML = `${player1.name}`
 hp1.innerHTML = `HP: ${player1.hp}`
 attack1.innerHTML = `Attack Power: ${player1.attack}`
 defense1.innerHTML = `Defense Power: ${player1.defense}`
 
+player2Tag.innerHTML = `${player2.name}`
 hp2.innerHTML = `HP: ${player2.hp}`
 attack2.innerHTML = `Attack Power: ${player2.attack}`
 defense2.innerHTML = `Defense Power: ${player2.defense}`
